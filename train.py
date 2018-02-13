bca52e9f18e49f612d5880b5bb4ee916e9e8055b
DeepInEvil <deba.kgec@gmail.com>
2018-02-13 11:32:01 +0100

    adding iteration

--- train.py
+++ train.py
@@ -23,8 +23,9 @@ def train(train_iter, dev_iter, vocab, model, args):
     last_step = 0
     model.train()
     for epoch in range(1, args.epochs+1):
-        for batch in train_iter:
-            feature, target = batch.text, batch.labels
+        for iter, traindata in enumerate(train_iter):
+            feature, target = traindata
+            print target
             feature.data.t_(), target.data.sub_(1)  # batch first, index align
             if args.cuda:
                 feature, target = feature.cuda(), target.cuda()
@@ -33,7 +34,7 @@ def train(train_iter, dev_iter, vocab, model, args):
             #topic_vec = torch.autograd.Variable(torch.from_numpy(np.random.rand(feature.size(0), 50)))
             #print topic_vec.size()
             #topic_vec = topic_vec.type(torch.FloatTensor)
-            optimizer.zero_grad()
+            model.zero_grad()
             #logit = model(feature, topic_vec)
             logit = model(feature)
 
@@ -42,15 +43,15 @@ def train(train_iter, dev_iter, vocab, model, args):
             optimizer.step()
 
             steps += 1
-            if steps % args.log_interval == 0:
+            if iter % args.log_interval == 0:
                 corrects = (torch.max(logit, 1)[1].view(target.size()).data == target.data).sum()
-                accuracy = 100.0 * corrects/batch.batch_size
+                accuracy = 100.0 * corrects/traindata.batch_size
                 sys.stdout.write(
-                    '\rBatch[{}] - loss: {:.6f}  acc: {:.4f}%({}/{})'.format(steps, 
+                    '\rBatch[{}] - loss: {:.6f}  acc: {:.4f}%({}/{})'.format(steps,
                                                                              loss.data[0], 
                                                                              accuracy,
                                                                              corrects,
-                                                                        batch.batch_size))
+                                                                        traindata.batch_size))
             if steps % args.test_interval == 0:
                 dev_acc = eval(dev_iter, model, args)
                 if dev_acc > best_acc:
