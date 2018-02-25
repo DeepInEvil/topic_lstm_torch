b48e4d10cca84ed805b011756e4630d9e1625ffe
DeepInEvil <deba.kgec@gmail.com>
2018-02-25 23:16:37 +0100

    changing directory for data file

--- model.py
+++ model.py
@@ -85,6 +85,7 @@ class RNN(nn.Module):
 
         #last_tensor=out_rnn[row_indices, col_indices, :]
         last_tensor = ht[-1]
+        print last_tensor.size()
         #fc_input = torch.mean(last_tensor, dim=1)
         #last_tensor = ht[-1]
         fc_input = self.bn2(last_tensor)
