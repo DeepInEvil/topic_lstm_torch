4ad8c871b76cfef1185c5736489b54c2fc7b6c16
DeepInEvil <deba.kgec@gmail.com>
2018-02-26 00:10:58 +0100

    changing directory for data file

--- model.py
+++ model.py
@@ -74,7 +74,7 @@ class RNN(nn.Module):
 
         yhat = []
         for j in range(x_embed.size(1)):
-            input_t = (x_embed[:, j: j + 1], 1)
+            input_t = x_embed[:, j: j + 1]
             print input_t.size()
             hx, cx = self.rnncell(input_t, (hx, cx))
             # print hx.size()
