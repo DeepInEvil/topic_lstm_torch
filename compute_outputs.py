32a41e1227ef66f6004857ad201f4626f8e6f4eb
DeepInEvil <deba.kgec@gmail.com>
2018-03-13 01:04:19 +0100

    compute stats

---
+++ compute_outputs.py
@@ -0,0 +1,39 @@
+import os
+import json
+from scipy import stats
+
+
+def convert_text2dict(filename):
+    last_line = file(filename, "r").readlines()[-1]
+    json_acceptable_string = last_line.replace('\"', '').replace('\n', '').replace("'", "\"")
+    out_dict = json.loads(json_acceptable_string)
+    return out_dict
+
+
+def calculate_stats(dir):
+    books = []
+    elecs = []
+    dvds = []
+    ktchn = []
+    files = os.listdir(dir)
+    for file in files:
+        out_dict = (dir + '/' + file)
+        books.append(out_dict['books'])
+        elecs.append(out_dict['electronics'])
+        dvds.append(out_dict['dvd'])
+        ktchn.append(out_dict['kitchen'])
+
+    print 'getting stats for Books:'
+    print stats.describe(books)
+    print 'getting stats for Electronics:'
+    print stats.describe(elecs)
+    print 'getting stats for Kitchen:'
+    print stats.describe(ktchn)
+    print 'getting stats for DVD:'
+    print stats.describe(dvds)
+
+if __name__ == '__main__':
+    print "stats on domains mit topic lstm"
+    calculate_stats('/home/DebanjanChaudhuri/topic_lstm_torch/mit_topic')
+    print "stats on domains ohne topic lstm"
+    calculate_stats('/home/DebanjanChaudhuri/topic_lstm_torch/ohne_topic')
